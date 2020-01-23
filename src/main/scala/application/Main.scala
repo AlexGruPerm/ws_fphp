50838db5b017f140c01c2863d6adedb34c0b8af5
AlexGruPerm <ugr@bk.ru>
2020-01-23 11:19:37 +0500

    Rewritten method run with foldM and StackTrace output.

--- src/main/scala/application/Main.scala
+++ src/main/scala/application/Main.scala
@@ -4,8 +4,8 @@ import confs.{Config, Configuration}
 import enironments.env.{AppTaskRes, RunResType}
 import org.slf4j.LoggerFactory
 import pureconfig.error.ConfigReaderFailures
-import zio.console.{putStrLn}
-import zio.{Task}
+import zio.{Task, UIO, URIO}
+import zio.console.{putStr, putStrLn}
 
 /**
  * https://zio.dev/docs/overview/overview_index
@@ -18,17 +18,13 @@ object Main extends zio.App {
   def run(args: List[String]): RunResType[Int] = {
     val logger = LoggerFactory.getLogger(getClass.getName)
     logger.info("Method run of Main")
-    WsApp(args).fold(
-      f => {
-        logger.error(s"Fail PgResearch.run f=$f msg=${f.getMessage} cause=${f.getCause}")
-        f.getStackTrace.foreach(_ => logger.error(toString))
-        0
-      },
-      s => {
-        logger.info(s"Success. End web service. $s")
-        1
-      }
-    )
+    WsApp(args).foldM(
+        throwable => putStrLn(s"Error: ${throwable.getMessage}") *> URIO.foreach(throwable.getStackTrace) {
+          sTraceRow => putStrLn(s"$sTraceRow")
+          }.map(_ => UIO.succeed(1)).flatten,
+        _ => UIO.succeed(0)
+      )
+
   }
 
   /**
@@ -39,21 +35,21 @@ object Main extends zio.App {
   private val WsApp: List[String] => AppTaskRes[Int]  = args =>
     for {
       _ <- putStrLn("[1]Web service starting...")
+      _ <- if (args.length < 0) Task.fail(new IllegalArgumentException("Need config file as parameter."))
+      else UIO.succeed(()) //If args.length correct just return succeed effect
       conf: Either[ConfigReaderFailures, Config] <-
-        Configuration.config.loadFile("C:\\ws_fphp\\src\\main\\resources\\application.conf") //todo: args[0] how in ZIO?
+        Configuration.config.loadFile("C:\\ws_fphp\\src\\main\\resources\\application.conf")
       res <- conf.fold(
         FailConfig => {
           println(s"Can't load config file. Error ${FailConfig.toString}")
-          Task(0)
+          UIO.succeed(0)
         },
         SuccessConf => {
           val dbConf = SuccessConf.dbConfig
           println(s"[2]Successful read config file. DB type = ${dbConf.dbtype} url = ${dbConf.url}")
           application.WsServer(SuccessConf)
         }
-      )//.forever.fork
-      //
-      //
+      )
       _ <- putStrLn("[7] Web service stopping...")
     } yield res
 
