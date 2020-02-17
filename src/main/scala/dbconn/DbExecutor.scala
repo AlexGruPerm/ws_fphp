9cc7c2255dcaa169d7fa3ae7915df91edf80e474
AlexGruPerm <ugr@bk.ru>
2020-02-17 18:25:20 +0500

    executing multiple queries parallel and combine results into single json.

---
+++ src/main/scala/dbconn/DbExecutor.scala
@@ -0,0 +1,24 @@
+package dbconn
+
+import confs.DbConfig
+import data.DbErrorDesc
+import reqdata.Dict
+import io.circe.generic.JsonCodec, io.circe.syntax._
+
+object DbExecutor {
+
+  private val failJson = DbErrorDesc("error",
+    "remaining connection slots are reserved for non-replication superuser connections",
+    "Cause of exception",
+    "PSQLException"
+  ).asJson
+
+  //each dict as Json result has a name f.e. "name" : "period"
+  /*
+ val execProcGetCursorData : (DbConfig,Dict) => = (conf, dict) => {
+
+ }
+  */
+
+
+}
