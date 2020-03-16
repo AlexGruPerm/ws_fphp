3aca2e08c1d40d74ca4080641f71c671fd54116c
AlexGruPerm <ugr@bk.ru>
2020-03-16 13:50:54 +0500

    rc18-2, new version of zio-logging. Bug related with unsafeRunToFuture is fixed. New tag here 0.6

--- src/main/scala/application/Main.scala
+++ src/main/scala/application/Main.scala
@@ -1,22 +1,13 @@
 package application
 
+import zio.App
 import confs.Configuration
 import envs.EnvContainer.ZEnvLog
 import zio.logging._
-import zio.{Task, UIO, ZEnv, ZIO, ZLayer}
-import zio.logging.{LogLevel, Logging}
+import zio.{Task, UIO, ZEnv, ZIO}
 import zio.console.putStrLn
 
-/**
- * https://zio.dev/docs/overview/overview_index
- * Apache DBCP : https://devcenter.heroku.com/articles/database-connection-pooling-with-scala
- * timout on effects  https://zio.dev/docs/overview/overview_basic_concurrency
- *    Timeout ZIO lets you timeout any effect using the ZIO#timeout method
- */
-
-object Main extends zio.App {
-
-
+object Main extends App {
   private def checkArgs : List[String] => ZIO[ZEnv,Throwable,Unit] = args => for {
     checkRes <- if (args.length < 0) {
       Task.fail(new IllegalArgumentException("Need config file as parameter."))}
@@ -24,17 +15,14 @@ object Main extends zio.App {
     }
   } yield checkRes
 
-
   private def wsApp: List[String] => ZIO[ZEnvLog, Throwable, Unit] = args =>
     for {
       _ <- logInfo("Web service starting")
       _ <- checkArgs(args)
       cfg <- Configuration.config.load("C:\\ws_fphp\\src\\main\\resources\\application.conf")
-      //cfg <- Configuration.config.load("/home/gdev/data/home/data/PROJECTS/ws_fphp/src/main/resources/application.conf")
       res <- WsServObj.WsServer(cfg)
       _ <- logInfo("Web service stopping")
     } yield res
-
 
   override def run(args: List[String]): ZIO[ZEnv, Nothing, Int] =
     wsApp(args).provideCustomLayer(envs.EnvContainer.ZEnvLogLayer)
@@ -44,5 +32,13 @@ object Main extends zio.App {
             } as 1,
         _ =>  putStrLn(s"Success exit of application.") as 0
       )
-
 }
+
+
+//cfg <- Configuration.config.load("/home/gdev/data/home/data/PROJECTS/ws_fphp/src/main/resources/application.conf")
+/**
+ * https://zio.dev/docs/overview/overview_index
+ * Apache DBCP : https://devcenter.heroku.com/articles/database-connection-pooling-with-scala
+ * timout on effects  https://zio.dev/docs/overview/overview_basic_concurrency
+ *    Timeout ZIO lets you timeout any effect using the ZIO#timeout method
+ */
